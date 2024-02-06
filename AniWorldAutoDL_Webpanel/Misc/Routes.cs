01f538c11def6e6db5bd15a9786f9c71858cb17f
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-02-07 00:49:00 +0100

    [~] Dashboard styling

--- AniWorldAutoDL_Webpanel/Misc/Routes.cs
+++ AniWorldAutoDL_Webpanel/Misc/Routes.cs
@@ -6,5 +6,6 @@
         public const string Dashboard = "/dashboard";
         public const string Settings = "/settings";
         public const string Shutdown = "/shutdown";
+        public const string ConverterSettings = "/convertersettings";
     }
 }
