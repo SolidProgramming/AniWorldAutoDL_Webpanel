249b3892f86e162ad4311f2e24d69647ffc81284
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-04-16 18:12:41 +0200

    [+] Proxy Settings

--- AniWorldAutoDL_Webpanel/Models/DownloaderPreferencesModel.cs
+++ AniWorldAutoDL_Webpanel/Models/DownloaderPreferencesModel.cs
@@ -6,9 +6,11 @@ namespace AniWorldAutoDL_Webpanel.Models
     {
         [Required(ErrorMessageResourceType = typeof(int), ErrorMessage = "Bitte eine Zahl eingeben")]
         public int Interval { get; set; }
-
         public bool AutoStart { get; set; }
-
         public bool TelegramCaptchaNotification { get; set; }
+        public bool UseProxy { get; set; }
+        public string? ProxyUri { get; set; }
+        public string? ProxyUsername { get; set; }
+        public string? ProxyPassword { get; set; }
     }
 }
